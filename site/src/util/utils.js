097738d25c12aad7eb8116dfa6b2a1755dceecd9
obonobo <ethanbnb@gmail.com>
2021-04-26 16:42:20 -0400

    WASM logo

--- site/src/util/utils.js
+++ site/src/util/utils.js
@@ -1,8 +1,47 @@
 const weAreInProduction = process.env.NODE_ENV === "production";
 const assetPrefix = weAreInProduction ? "/preprosql" : "";
 
+const urlFor = (path = "/") => `${assetPrefix}${path}`;
+
 const redirect = (path = "/") =>
   window && window.location && window.location.replace(`${assetPrefix}${path}`);
 
+/**
+ * Convert template literal backtick-strings to React style objects for use as
+ * inline-styles.
+ *
+ * Utility template literal parser
+ *
+ * @param {string} str
+ * @returns
+ */
+const style = (str = "") => {
+  const s = {};
+  const myString = (str instanceof String ? str : str.join(""))
+    .replace(/ {2,}/g, "")
+    .replace(/\n/g, "");
+
+  const formatStringToCamelCase = (someString) => {
+    const splitted = someString.split("-");
+    if (splitted.length === 1) return splitted[0];
+    return (
+      splitted[0] +
+      splitted
+        .slice(1)
+        .map((word) => word[0].toUpperCase() + word.slice(1))
+        .join("")
+    );
+  };
+
+  myString.split(";").forEach((el) => {
+    const [property, value] = el.split(":");
+    if (!property) return;
+    const formattedProperty = formatStringToCamelCase(property.trim());
+    s[formattedProperty] = value.trim();
+  });
+
+  return s;
+};
+
 export default weAreInProduction;
-export { weAreInProduction, assetPrefix, redirect };
+export { weAreInProduction, assetPrefix, redirect, style, urlFor };

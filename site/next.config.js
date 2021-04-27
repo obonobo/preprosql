28d967f724a93e9468a21472d627a676b6d489f0
obonobo <ethanbnb@gmail.com>
2021-04-27 15:08:53 -0400

    Upgraded configs, removed file-loader dep

--- site/next.config.js
+++ site/next.config.js
@@ -5,33 +5,10 @@ module.exports = {
   basePath: process.env.NODE_ENV === "production" ? "/preprosql" : "",
   assetPrefix: process.env.NODE_ENV === "production" ? "/preprosql/" : "/",
   webpack: (config) => {
-    config.module.rules.push(
-      {
-        // Videos
-        test: /\.mp4$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "video",
-            },
-          },
-        ],
-      },
-      {
-        // Images
-        test: /\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-            },
-          },
-        ],
-      }
-    );
+    config.module.rules.push({
+      test: /\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/i,
+      type: "asset/resource",
+    });
     return config;
   },
 };

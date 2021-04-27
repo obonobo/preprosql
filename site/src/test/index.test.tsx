81e0068c4100028a61e0842ab24870c73d0b29e2
obonobo <ethanbnb@gmail.com>
2021-04-27 17:46:54 -0400

    Converted the tests to TypeScript

---
+++ site/src/test/index.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import App from "../../pages/index";
+
+describe("App", () => {
+  it("renders without crashing", () => {
+    render(<App />);
+  });
+});
